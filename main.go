50347a39e70549ce8713c512bd4574092df81678
PragmaticNick <pragmatic.nick@gmail.com>
2026-02-16 18:17:11 +0100

    get rid of gin
    Signed-off-by: PragmaticNick <pragmatic.nick@gmail.com>

--- main.go
+++ main.go
@@ -4,13 +4,12 @@ import (
 	"crypto/sha256"
 	"database/sql"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	_ "modernc.org/sqlite"
-
-	"github.com/gin-gonic/gin"
 )
 
 const (
@@ -34,96 +33,100 @@ func main() {
 		log.Fatalf("failed to create table [links]: %s", err.Error())
 	}
 
-	http
-
-	server := gin.Default()
-
-	server.POST("/shorten", shorten(db))
-	server.GET("/:hash", get(db))
-	err = server.Run(addr)
-	if err != nil {
-		log.Fatalf("failed to run HTTP server: %s", err.Error())
-	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/shorten", shorten(db))
+	mux.HandleFunc("/", get(db))
 }
+
+type H = map[string]string
 
 type input struct {
 	OriginalLink string `json:"originalLink"`
 }
 
-func shorten(db *sql.DB) func(*gin.Context) {
-	return func(c *gin.Context) {
+func shorten(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		d := json.NewDecoder(r.Body)
 		body := input{}
-		if err := c.ShouldBindJSON(&body); err != nil {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid input format"})
+		if err := d.Decode(&body); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
-		tx, err := db.Begin()
+		hash, err := saveLink(db, body.OriginalLink)
 		if err != nil {
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-		defer tx.Rollback()
-
-		result, err := tx.Exec("INSERT INTO links (original) values (?)", body.OriginalLink)
-		if err != nil {
-			log.Printf("failed to insert original link: %s", err.Error())
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-
-		id, err := result.LastInsertId()
-		if err != nil {
-			log.Printf("failed to get id: %s", err.Error())
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-
-		input := fmt.Sprintf("%s%d", body.OriginalLink, id)
-		hashFunc := sha256.New()
-		hashFunc.Write([]byte(input))
-		hash := base64.URLEncoding.EncodeToString(hashFunc.Sum(nil))[:10]
-
-		_, err = tx.Exec("UPDATE links SET hash = ? WHERE id = ?", hash, id)
-		if err != nil {
-			log.Printf("failed to set hash: %s", err.Error())
-			c.AbortWithStatus(http.StatusInternalServerError)
-			return
-		}
-
-		if err := tx.Commit(); err != nil {
-			log.Printf("failed to commit transaction: %s", err.Error())
-			c.AbortWithStatus(http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		shortLink := fmt.Sprintf("%s/%s", addr, hash)
-		c.JSON(http.StatusCreated, gin.H{"shortLink": shortLink})
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(H{"shortlink": shortLink})
 	}
 }
 
-func get(db *sql.DB) func(*gin.Context) {
-	return func(c *gin.Context) {
-		hash, ok := c.Params.Get("hash")
-		if !ok {
-			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "hash not presented"})
-			return
-		}
+func get(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		hash := r.URL.Path
 
 		var original string
 		row := db.QueryRow("SELECT original FROM links WHERE hash = ?", hash)
 		err := row.Scan(&original)
 		if err != nil {
 			if err == sql.ErrNoRows {
-				c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "original link not found"})
+				w.WriteHeader(http.StatusNotFound)
+				json.NewEncoder(w).Encode(H{"error": "original link not found"})
 				return
 			}
-
-			c.AbortWithStatus(http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		c.Redirect(http.StatusFound, original)
-
+		http.Redirect(w, r, original, http.StatusFound)
 	}
+}
+
+func saveLink(db *sql.DB, original string) (string, error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return "", err
+	}
+	defer tx.Rollback()
+
+	result, err := tx.Exec("INSERT INTO links (original) values (?)", original)
+	if err != nil {
+		log.Printf("failed to insert original link: %s", err.Error())
+		return "", err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		log.Printf("failed to get id: %s", err.Error())
+		return "", err
+	}
+
+	input := fmt.Sprintf("%s%d", original, id)
+	hashFunc := sha256.New()
+	hashFunc.Write([]byte(input))
+	hash := base64.URLEncoding.EncodeToString(hashFunc.Sum(nil))[:10]
+
+	_, err = tx.Exec("UPDATE links SET hash = ? WHERE id = ?", hash, id)
+	if err != nil {
+		log.Printf("failed to set hash: %s", err.Error())
+		return "", err
+	}
+
+	if err := tx.Commit(); err != nil {
+		log.Printf("failed to commit transaction: %s", err.Error())
+		return "", err
+	}
+
+	return hash, nil
 }
